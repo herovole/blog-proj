f26eb32c332179b57bdef7d69d06c062a2de5833
Herovole <developerTools201812@gmail.com>
2025-02-21 16:51:46 +0900

    20250221 deploy

--- deploy/staging/scripts/build_run_backend.sh
+++ deploy/staging/scripts/build_run_backend.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 # build_and_run_backend.sh
 
+set -e  # Stop on error
+
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
+source $AWS_USER_EC2
+
+echo $(date) deploying backend container $DOCKER_BACKEND_CONTAINER | tee -a $LOG_FILE
 
 # Check if the container exists (running or stopped)
-if docker ps -a --format \'{{.Names}}\' | grep -wq \"$DOCKER_BACKEND_CONTAINER\"; then
-    echo \"Stopping and removing container \'$DOCKER_BACKEND_CONTAINER\'...\"
-    docker stop \"$DOCKER_BACKEND_CONTAINER\" && docker rm \"$DOCKER_BACKEND_CONTAINER\"
+if sudo docker ps -a --format \'{{.Names}}\' | grep -wq \"$DOCKER_BACKEND_CONTAINER\"; then
+    echo $(date) \"Stopping and removing container \'$DOCKER_BACKEND_CONTAINER\'...\" | tee -a $LOG_FILE
+    sudo docker stop \"$DOCKER_BACKEND_CONTAINER\" && docker rm \"$DOCKER_BACKEND_CONTAINER\"
 else
-    echo \"Container \'$DOCKER_BACKEND_CONTAINER\' does not exist. Skipping removal.\"
+    echo $(date) \"Container \'$DOCKER_BACKEND_CONTAINER\' does not exist. Skipping removal.\" | tee -a $LOG_FILE
 fi
 
 # Pull the backend image from ECR
+echo $(date) pulling backend image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_BACKEND_IMAGE:latest | tee -a $LOG_FILE
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_BACKEND_IMAGE:latest
+
+if sudo docker images | grep -q $DOCKER_BACKEND_IMAGE; then
+    echo $(date) "Image pulled out ${DOCKER_BACKEND_IMAGE}." | tee -a $LOG_FILE
+else
+    echo $(date) "No image ${DOCKER_BACKEND_IMAGE}." | tee -a $LOG_FILE
+    exit 1
+fi
 
 # Run the backend container
 sudo docker run -d \
@@ -21,3 +34,11 @@ sudo docker run -d \
         --net $DOCKER_NETWORK \
         -p 8080:8080 \
         $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_BACKEND_IMAGE:latest
+
+# Verify backend container running
+if sudo docker ps | grep $DOCKER_BACKEND_CONTAINER; then
+    echo $(date) "${DOCKER_BACKEND_CONTAINER} deployed" | tee -a $LOG_FILE
+else
+    echo $(date) "${DOCKER_BACKEND_CONTAINER} failed to get deployed" | tee -a $LOG_FILE
+    exit 1
+fi
--- deploy/staging/scripts/login_ecr.sh
+++ deploy/staging/scripts/login_ecr.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e  # Stop on error
 
-
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
 
+echo $(date) Downloading S3 aws_user file. | tee -a $LOG_FILE
 # Download the file from S3
-
-aws s3 ls s3://blog2025-stg-s3-private/
-aws s3 ls s3://blog2025-stg-s3-private/build/
-
 aws s3 cp ${AWS_USER_S3} ${AWS_USER_EC2}
 
 # Read the file
 source $AWS_USER_EC2
 
+echo $(date) Login to ECR. | tee -a $LOG_FILE
 # Authenticate to ECR
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
