a75a9956e9c4f30c7aca8c723c8e835a8a83ab92
Herovole <developerTools201812@gmail.com>
2025-02-21 16:51:46 +0900

    20250221 deploy

--- deploy/staging/scripts/build_run_frontend.sh
+++ deploy/staging/scripts/build_run_frontend.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # build_and_run_frontend.sh
 
+set -e  # Stop on error
+
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
+source $AWS_USER_EC2
 
 # Check if the container exists (running or stopped)
 if docker ps -a --format \'{{.Names}}\' | grep -wq \"$DOCKER_FRONTEND_CONTAINER\"; then
@@ -15,9 +18,17 @@ fi
 # Pull the frontend image from ECR
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_FRONTEND_IMAGE:latest
 
+if ! sudo docker images | grep -q $DOCKER_FRONTEND_IMAGE; then
+    echo "No image after pull."
+    exit 1
+fi
+
 # Run the frontend container
 sudo docker run -d \
         --name $DOCKER_FRONTEND_CONTAINER \
         --net $DOCKER_NETWORK \
         -p 443:443 \
         $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_FRONTEND_IMAGE:latest
+
+# Verify frontend container running
+sudo docker ps | grep $DOCKER_FRONTEND_CONTAINER
--- deploy/staging/scripts/setup_docker_network.sh
+++ deploy/staging/scripts/setup_docker_network.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e  # Stop on error
+
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
 
 # Check if the network exists
 if sudo docker network ls --format \'{{.Name}}\' | grep -wq \"$DOCKER_NETWORK\"; then
-    echo \"Network \'$DOCKER_NETWORK\' already exists. Skipping creation.\"
+    echo $(date) docker network ${DOCKER_NETWORK} already exists. Skipping creation. | tee -a $LOG_FILE
 else
-    echo \"Creating Docker network \'$DOCKER_NETWORK\'...\"
+    echo $(date) Creating docker network ${DOCKER_NETWORK}. | tee -a $LOG_FILE
     sudo docker network create \"$DOCKER_NETWORK\"
 fi
