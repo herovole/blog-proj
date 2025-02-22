a9954c2cb940052e821d730346ad706247c844a0
Herovole <developerTools201812@gmail.com>
2025-02-22 19:22:32 +0900

    20250222 deploy

--- deploy/staging/scripts/verify_containers.sh
+++ deploy/staging/scripts/verify_containers.sh
@@ -6,10 +6,10 @@ set -e  # Stop on error
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
 
-echo verify backend container running
+echo verifying backend container running
 # Verify backend container is running
 sudo docker ps | grep $DOCKER_BACKEND_CONTAINER
 
-echo verify frontend container running
+echo verifying frontend container running
 # Verify frontend container is running
 sudo docker ps | grep $DOCKER_FRONTEND_CONTAINER
