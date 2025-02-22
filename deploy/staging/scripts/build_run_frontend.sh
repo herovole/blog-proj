aaf8fbec903af2bac3fc459ea04d02ee51170123
Herovole <developerTools201812@gmail.com>
2025-02-22 19:22:32 +0900

    20250222 deploy

--- deploy/staging/scripts/build_run_frontend.sh
+++ deploy/staging/scripts/build_run_frontend.sh
@@ -7,19 +7,24 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 source $SCRIPT_DIR/deploy_config.log
 source $AWS_USER_EC2
 
+echo $(date) deploying frontend container $DOCKER_FRONTEND_CONTAINER | tee -a $LOG_FILE
+
 # Check if the container exists (running or stopped)
-if docker ps -a --format \'{{.Names}}\' | grep -wq \"$DOCKER_FRONTEND_CONTAINER\"; then
-    echo \"Stopping and removing container \'$DOCKER_FRONTEND_CONTAINER\'...\"
-    docker stop \"$DOCKER_FRONTEND_CONTAINER\" && docker rm \"$DOCKER_FRONTEND_CONTAINER\"
+if sudo docker ps -a --format \'{{.Names}}\' | grep -wq \"$DOCKER_FRONTEND_CONTAINER\"; then
+    echo $(date) \"Stopping and removing container \'$DOCKER_FRONTEND_CONTAINER\'...\" | tee -a $LOG_FILE
+    sudo docker stop \"$DOCKER_FRONTEND_CONTAINER\" && docker rm \"$DOCKER_FRONTEND_CONTAINER\"
 else
-    echo \"Container \'$DOCKER_FRONTEND_CONTAINER\' does not exist. Skipping removal.\"
+    echo $(date) \"Container \'$DOCKER_FRONTEND_CONTAINER\' does not exist. Skipping removal.\" | tee -a $LOG_FILE
 fi
 
 # Pull the frontend image from ECR
+echo $(date) pulling frontend image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_FRONTEND_IMAGE:latest | tee -a $LOG_FILE
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_FRONTEND_IMAGE:latest
 
-if ! sudo docker images | grep -q $DOCKER_FRONTEND_IMAGE; then
-    echo "No image after pull."
+if sudo docker images | grep -q $DOCKER_FRONTEND_IMAGE; then
+    echo $(date) "Image pulled out ${DOCKER_FRONTEND_IMAGE}." | tee -a $LOG_FILE
+else
+    echo $(date) "No image ${DOCKER_FRONTEND_IMAGE}." | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -31,4 +36,9 @@ sudo docker run -d \
         $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_FRONTEND_IMAGE:latest
 
 # Verify frontend container running
-sudo docker ps | grep $DOCKER_FRONTEND_CONTAINER
+if sudo docker ps | grep $DOCKER_FRONTEND_CONTAINER; then
+    echo $(date) "${DOCKER_FRONTEND_CONTAINER} deployed" | tee -a $LOG_FILE
+else
+    echo $(date) "${DOCKER_FRONTEND_CONTAINER} failed to get deployed" | tee -a $LOG_FILE
+    exit 1
+fi
--- deploy/staging/scripts/login_ecr.sh
+++ deploy/staging/scripts/login_ecr.sh
@@ -11,6 +11,6 @@ aws s3 cp ${AWS_USER_S3} ${AWS_USER_EC2}
 # Read the file
 source $AWS_USER_EC2
 
-echo $(date) Login to ECR. | tee -a $LOG_FILE
+echo $(date) Login to ECR. $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com | tee -a $LOG_FILE
 # Authenticate to ECR
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr get-login-password --region $AWS_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
