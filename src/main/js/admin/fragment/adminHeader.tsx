23b3828b32a5157a793a99233fd6a340db426d8c
Herovole <developerTools201812@gmail.com>
2025-03-03 14:52:29 +0900

    20250303 herobanner

--- src/main/js/admin/fragment/adminHeader.tsx
+++ src/main/js/admin/fragment/adminHeader.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import {NavLink} from "react-router-dom"; // 追加
+import {NavLink} from "react-router-dom";
+import {ResourceManagement} from "../../service/resourceManagement"; // 追加
 
 export const AdminHeader: React.FC = () => {
+    const [resourcePrefix, setResourcePrefix] = React.useState<string | null>(null);
+
+    React.useEffect(() => {
+        ResourceManagement.getInstance().systemImagePrefixWithSlash().then(setResourcePrefix);
+    }, [resourcePrefix]);
+
     return (
         <div className="header-frame">
+            <div className="hero-banner-base">
+                <img src={resourcePrefix + "hero_banner.jpg"}
+                     alt={"Being registered"}/>
+            </div>
             <div className="header-alignment">
                 <NavLink className={({isActive}) => (isActive ? "header-unit-active" : "header-unit")}
                          to="/" end>【公開】トップ</NavLink>
