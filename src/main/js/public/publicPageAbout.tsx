1322c0a20a343942a90952305026c37927ab6a47
Herovole <developerTools201812@gmail.com>
2025-03-05 19:37:48 +0900

    20250305 markdown

--- src/main/js/public/publicPageAbout.tsx
+++ src/main/js/public/publicPageAbout.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
+import {marked} from "marked"; // Import the Markdown parser
 
 export const PublicPageAbout = () => {
-    const [content, setContent] = useState("");
+    const [content, setContent] = useState<string>("");
 
     useEffect(() => {
         fetch("/content/about.md")
             .then((res) => res.text())
-            .then((text) => setContent(text));
+            .then((text) => setContent(marked(text) as string))  // Convert Markdown to HTML
     }, []);
 
-    return <div dangerouslySetInnerHTML={{ __html: content }} />;
+    return <div dangerouslySetInnerHTML={{__html: content}}/>;
 };
