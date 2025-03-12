2957b78c88b64b572d3cca6286a7ec8b3735199c
Herovole <developerTools201812@gmail.com>
2025-03-12 16:39:49 +0900

    20250312 sql

--- schema/a_topic_tag.sql
+++ schema/a_topic_tag.sql
@@ -8,7 +8,7 @@ CREATE TABLE a_topic_tag (
   delete_flag TINYINT(1) NOT NULL DEFAULT 0
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-INSERT INTO a_tag (id,name_en,name_ja) values ('1','test','テスト');
-INSERT INTO a_tag (id,name_en,name_ja) values ('2','news','時事');
-INSERT INTO a_tag (id,name_en,name_ja) values ('3','society','社会');
-INSERT INTO a_tag (id,name_en,name_ja) values ('4','country','国');
+INSERT INTO a_topic_tag (id,name_en,name_ja) values ('1','test','テスト');
+INSERT INTO a_topic_tag (id,name_en,name_ja) values ('2','news','時事');
+INSERT INTO a_topic_tag (id,name_en,name_ja) values ('3','society','社会');
+INSERT INTO a_topic_tag (id,name_en,name_ja) values ('4','country','国');
