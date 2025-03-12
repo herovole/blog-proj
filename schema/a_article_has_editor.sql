9e973f5a5af9c251c3fd9d4764cf3e43c5f27553
Herovole <developerTools201812@gmail.com>
2025-03-12 16:39:49 +0900

    20250312 sql

--- schema/a_article_has_editor.sql
+++ schema/a_article_has_editor.sql
@@ -6,6 +6,6 @@ CREATE TABLE a_article_has_editor (
   insert_timestamp timestamp default current_timestamp,
 
   FOREIGN KEY (article_id) REFERENCES a_article(id) ON DELETE CASCADE,
-  FOREIGN KEY (editor_id) REFERENCES m_editor(id) ON DELETE CASCADE
+  FOREIGN KEY (editor_id) REFERENCES m_admin_user(id) ON DELETE CASCADE
 
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
