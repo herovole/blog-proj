acb6ec6fe46050eafdc334f1c73b85ae64a18cd3
Herovole <developerTools201812@gmail.com>
2025-03-15 17:56:36 +0900

    20250315 comment modal design, enable recaptcha

--- src/main/java/org/herovole/blogproj/application/auth/verifyorganicity/VerifyOrganicity.java
+++ src/main/java/org/herovole/blogproj/application/auth/verifyorganicity/VerifyOrganicity.java
@@ -33,9 +33,7 @@ public class VerifyOrganicity {
                 input.getVerificationToken(),
                 input.getIPv4Address()
         );
-        //FIXME : temporarily suppresses to block the process for the testing purposes
-        probabilityOfBeingHuman = 1.0f;
-        //if (probabilityOfBeingHuman == null) throw new IllegalStateException();
+        if (probabilityOfBeingHuman == null) throw new IllegalStateException();
         logger.info("User {} score {}", input.getUserId(), probabilityOfBeingHuman);
 
         if(probabilityOfBeingHuman < THRESHOLD) {
