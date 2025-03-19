4b32729e6a795ffdd9c59feefc6b98c0429668ba
Herovole <developerTools201812@gmail.com>
2025-03-19 09:46:58 +0900

    20250319 test script, update deployment

--- src/main/js/public/fragment/livedoorRss.tsx
+++ src/main/js/public/fragment/livedoorRss.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 export const LivedoorRss: React.FC = () => {
+    useEffect(() => {
+        // Set the blogroll channel ID dynamically
+        window.blogroll_channel_id = 356050;
 
-    const script = "<!-- var blogroll_channel_id = 356050; // -->";
-    return (<div>
-            <script type="text/javascript">
-                {script}
-            </script>
-            <script type="text/javascript" charSet="utf-8" src="https://blogroll.livedoor.net/js/blogroll.js"></script>
-            <link rel="stylesheet" type="text/css" href="https://blogroll.livedoor.net/css/default2.css"/>
+        // Create the script element for the blogroll script
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.charset = "utf-8";
+        script.src = "https://blogroll.livedoor.net/js/blogroll.js";
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div>
+            {/* Stylesheet for livedoor RSS widget */}
+            <link
+                rel="stylesheet"
+                type="text/css"
+                href="https://blogroll.livedoor.net/css/default2.css"
+            />
+            <div id="blogroll-container"></div>
         </div>
     );
+};
 
-}
