6e87f661f47ca58e4e0e77f57196e561eba80e8d
Herovole <developerTools201812@gmail.com>
2025-04-18 23:21:35 +0900

    handle a003, omit 4ch ref

--- src/main/js/public/fragment/articlelist/publicArticleHeadlinesIndividualLarge.tsx
+++ src/main/js/public/fragment/articlelist/publicArticleHeadlinesIndividualLarge.tsx
@@ -48,8 +48,8 @@ export const PublicArticleHeadlinesIndividualLarge: React.FC<PublicArticleHeadli
                                 searchBaseUrl={directoryToIndividualPage}/>
                 </div>
                 <div className="article-source-url">引用元: {article.sourceUrl}</div>
-                <div
-                    className="article-timestamp">引用元日付: {YyyyMMDd.valueOfYyyyMMDd(article.sourceDate).toYyyySlashMMSlashDd()}</div>
+                <div className="article-source-url">引用元: {article.sourceUrl.includes("4chan.org") ? "(出典元サイトにトラブル発生中のため一時的に非表示)" : article.sourceUrl}</div>
+                <div className="article-timestamp">引用元日付: {YyyyMMDd.valueOfYyyyMMDd(article.sourceDate).toYyyySlashMMSlashDd()}</div>
                 <div className="article-timestamp">ブログ内掲載: {article.registrationTimestamp}</div>
             </div>);
     }
