46bcf22c30fe8c2deeff4d897b6154abd52f846f
Herovole <developerTools201812@gmail.com>
2025-04-20 13:30:21 +0900

    handle a004, antenna

--- src/main/js/public/fragment/livedoorRss.tsx
+++ src/main/js/public/fragment/livedoorRss.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export const LivedoorRss: React.FC = () => {
 
     return (
-        <iframe style={{height:"700px"}} src="https://blogroll.livedoor.net/356050/iframe" allowTransparency="true"></iframe>
+        <iframe src="https://blogroll.livedoor.net/356050/iframe"
+                allowtransparency="false"
+                style="background: #cccc88;height:700px"></iframe>
     );
 }
