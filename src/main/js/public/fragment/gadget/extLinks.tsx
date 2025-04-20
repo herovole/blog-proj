17cc01154c936a4bcf10de0fa570844022a831ef
Herovole <developerTools201812@gmail.com>
2025-04-20 13:01:03 +0900

    handle a004, antenna

--- src/main/js/public/fragment/gadget/extLinks.tsx
+++ src/main/js/public/fragment/gadget/extLinks.tsx
@@ -5,6 +5,7 @@ export const ExtLinks: React.FC = () => {
     const [isOpen, setIsOpen] = React.useState<boolean>(true);
 
     const itemBlogWith2Net = <Link className="gadget-links-item-link" to="https://blog.with2.net/link/?id=2128848&cid=4281">人気ブログランキング</Link>
+    const itemYakutenaCom = <Link className="gadget-links-item-link" to="https://www.yakutena.com/">ヤクテナ</Link>
 
     if (isOpen) {
         return (
@@ -13,9 +14,13 @@ export const ExtLinks: React.FC = () => {
                     setIsOpen(false);
                 }}>▽参加リンクガジェットを畳む
                 </button>
-                <div className="gadget-links-title">当ブログの参加リンク</div>
+                <div className="gadget-links-title">登録アンテナサイト</div>
                 <div className="gadget-links-body">
                     <div className="gadget-links-item">{itemBlogWith2Net}</div>
+                </div>
+                <div className="gadget-links-title">申請中アンテナサイト</div>
+                <div className="gadget-links-body">
+                    <div className="gadget-links-item">{itemYakutenaCom}</div>
                 </div>
             </div>
         );
---
+++ src/main/js/public/fragment/publicAboutLinks.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+type PublicAboutLinks = {
+    iconPrefix: string;
+}
+
+
+export const PublicAboutLinks: React.FC<PublicAboutLinks> = ({iconPrefix}) => {
+
+    return (
+        <div>
+            <h2>RSS</h2>
+            <div className="indent">
+                <a href={"./rss.xml"}>
+                    <img src={iconPrefix + "Feed-icon.svg"} style={{cursor: 'pointer', width: 64, height: 64}}
+                         alt="rss"/>
+                    <span>RSS 2.0</span>
+                </a>
+                <br/>
+                <a href={"./rss10.xml"}>
+                    <img src={iconPrefix + "Feed-icon.svg"} style={{cursor: 'pointer', width: 64, height: 64}}
+                         alt="rss"/>
+                    <span>RSS 1.0</span>
+                </a>
+            </div>
+            <br/>
+            <h2>X(旧Twitter)</h2>
+            <div className="indent">
+                <a href={"https://x.com/at_archives_com"}>
+                    <img src={iconPrefix + "X_logo_2023.svg"} style={{cursor: 'pointer', width: 64, height: 64}}
+                         alt="x"/>
+                </a>
+            </div>
+            <br/>
+            <div>
+                <h2>登録アンテナサイト</h2>
+                <div className="indent">
+                    <div>
+                        <span>人気ブログランキング : </span>
+                        <a href="https://blog.with2.net/link/?id=2128848&cid=4281" title="海外の反応ランキング"
+                           target="_blank"><img alt="海外の反応ランキング" width="110" height="31"
+                                                src="https://blog.with2.net/img/banner/c/banner_1/br_c_4281_1.gif"/></a>
+                        <a href="https://blog.with2.net/link/?id=2128848&follow" title="人気ブログランキングでフォロー"
+                           target="_blank"><img alt="人気ブログランキングでフォロー" width="172" height="20"
+                                                src="https://blog.with2.net/banner/follow/2128848?t=m"/></a>
+                    </div>
+                </div>
+                <br/>
+                <h2>申請中アンテナサイト</h2>
+                <div className="indent">
+                    <span>ヤクテナ : <Link to="https://www.yakutena.com/">https://www.yakutena.com/</Link> </span>
+                </div>
+            </div>
+        </div>
+    );
+}
