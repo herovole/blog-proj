03573365dd16f24dbf351a746a15381efc67caae
herovole <developertools201812@gmail.com>
2025-04-28 17:00:08 +0900

    20250428 cancel request

--- src/main/js/public/fragment/gadget/extLinks.tsx
+++ src/main/js/public/fragment/gadget/extLinks.tsx
@@ -5,8 +5,8 @@ export const ExtLinks: React.FC = () => {
 
     const itemBlogWith2Net = <Link className="gadget-links-item-link"
                                    to="https://blog.with2.net/link/?id=2128848&cid=4281">人気ブログランキング</Link>;
-    const itemLivdirComKaigaiAntenna = <Link className="gadget-links-item-link"
-                                             to="https://livdir.com/kaigai-antenna/">シンプル海外の反応アンテナ</Link>
+    //const itemLivdirComKaigaiAntenna = <Link className="gadget-links-item-link"
+    //                                         to="https://livdir.com/kaigai-antenna/">シンプル海外の反応アンテナ</Link>
     const itemKaihanAntennaCom = <Link className="gadget-links-item-link"
                                        to="https://kaihan-antenna.com/">海外の反応アンテナ.com</Link>;
     const itemKaikenAtnaJp = <Link className="gadget-links-item-link"
@@ -21,7 +21,6 @@ export const ExtLinks: React.FC = () => {
             </div>
             <div className="gadget-links-title">申請中アンテナサイト</div>
             <div className="gadget-links-body">
-                <div className="gadget-links-item">{itemLivdirComKaigaiAntenna}</div>
                 <div className="gadget-links-item">{itemKaihanAntennaCom}</div>
                 <div className="gadget-links-item">{itemKaikenAtnaJp}</div>
                 <div className="gadget-links-item">{itemYakutenaCom}</div>
