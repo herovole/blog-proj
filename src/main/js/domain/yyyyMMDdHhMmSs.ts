81b5623f1912f342e2b0cb3b83cdc99dc6e58b5a
herovole <developertools201812@gmail.com>
2025-04-30 15:48:07 +0900

    handle i013, timestamp display

---
+++ src/main/js/domain/yyyyMMDdHhMmSs.ts
@@ -0,0 +1,32 @@
+export class YyyyMMDdHhMmSs {
+
+    public static valueOfYyyyMMDdHhMmSs(field: string): YyyyMMDdHhMmSs {
+        if (YyyyMMDdHhMmSs.isFourteenDigitNumber(field)) {
+            return new YyyyMMDdHhMmSs(field);
+        }
+        return new YyyyMMDdHhMmSs(null);
+    }
+
+    private static isFourteenDigitNumber(str: string) {
+        return /^\d{14,}$/.test(str)
+    };
+
+    private readonly _field: string | null;
+
+    constructor(field: string | null) {
+        this._field = field;
+    }
+
+    public isEmpty(): boolean {
+        return this._field == null;
+    }
+
+    public toYyyySlashMMSlashDdSpaceHhColonMm(): string {
+        if (this._field == null) { return ""; }
+        const yyyyMMDdHhMmSs = this._field;
+        return `${yyyyMMDdHhMmSs.substring(0, 4)}/
+        ${yyyyMMDdHhMmSs.substring(4, 6)}/
+        ${yyyyMMDdHhMmSs.substring(6, 8)} ${yyyyMMDdHhMmSs.substring(8, 10)}:
+        ${yyyyMMDdHhMmSs.substring(10, 12)}`;
+    }
+}
